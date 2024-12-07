5b215ed66f131ca652225e20623ff466b4ab91d3
Bilal Essofyany <soufyani.bilal@gmail.com>
2024-12-07 02:08:03 +0100

    feat: create icon button component

---
+++ src/components/ui/Button/IconButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="icon-button">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+.icon-button {
+  width: 36px;
+  height: 36px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: white;
+  border: 1px solid var(--color-soft-200);
+  color: var(--color-black-muted);
+  border-radius: var(--radius-8);
+  cursor: pointer;
+
+  &:focus {
+    border-color: var(--color-black);
+  }
+}
+</style>
