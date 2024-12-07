cfb5fc846ace7ae8f84f30da0b0a2f85c2ab20b1
Bilal Essofyany <soufyani.bilal@gmail.com>
2024-12-07 02:09:06 +0100

    feat: create city info component

---
+++ src/components/Weather/CityInfo.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import IconButton from '@/components/ui/Button/IconButton.vue'
+import Popover from '@/components/ui/popover/Popover.vue'
+import IconSettings from '@/components/icons/IconSettings.vue'
+import WeatherSettings from './WeatherSettings.vue'
+</script>
+
+<template>
+  <header class="city-info">
+    <div class="city-details">
+      <h1 class="city-name">Casablanca</h1>
+      <div class="datetime-info">
+        <p class="date-text">Friday, November 8, 2024</p>
+        <p class="time-text">9:09 PM</p>
+      </div>
+    </div>
+
+    <Popover>
+      <template #trigger>
+        <IconButton>
+          <IconSettings />
+        </IconButton>
+      </template>
+      <WeatherSettings />
+    </Popover>
+  </header>
+</template>
+
+<style scoped>
+.city-info {
+  display: flex;
+  justify-content: space-between;
+
+  .city-details {
+    display: flex;
+    flex-direction: column;
+    gap: 2;
+    .city-name {
+      font-size: 40px;
+      font-weight: 700;
+      color: var(--color-black);
+    }
+
+    .datetime-info {
+      display: flex;
+      flex-direction: column;
+      gap: 4;
+      font-weight: 500;
+      color: var(--color-black-muted);
+
+      .date-text {
+        font-size: 16px;
+      }
+      .time-text {
+        font-size: 14px;
+      }
+    }
+  }
+}
+</style>
