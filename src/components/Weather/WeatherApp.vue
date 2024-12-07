ff2b1294b5630a7446cb694bc7f41a8ca29d0008
Bilal Essofyany <soufyani.bilal@gmail.com>
2024-12-07 02:07:06 +0100

    feat: create home page with weather app component

---
+++ src/components/Weather/WeatherApp.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import CityInfo from './CityInfo.vue'
+import WeatherAQI from './WeatherAQI.vue'
+import WeatherForecast from './WeatherForecast.vue'
+import WeatherOverview from './WeatherOverview.vue'
+import WeatherStat from './WeatherStat.vue'
+</script>
+
+<template>
+  <div class="weather-card">
+    <CityInfo />
+    <WeatherOverview />
+    <WeatherStat />
+    <WeatherAQI />
+    <WeatherForecast />
+  </div>
+</template>
+
+<style scoped>
+.weather-card {
+  position: absolute;
+  flex-shrink: 0;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: white;
+  border-radius: var(--radius-24);
+  min-height: 60%;
+  max-height: 90%;
+  width: 75%;
+  max-width: 90%;
+  padding: 24px 32px;
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+}
+
+@media (max-width: 600px) {
+  .weather-card {
+    width: 90%;
+  }
+}
+
+@media (min-width: 1024px) {
+  .weather-card {
+    width: 40%;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import WeatherApp from '@/components/Weather/WeatherApp.vue'
+</script>
+
+<template>
+  <main>
+    <WeatherApp />
+  </main>
+</template>
+
+<style scoped>
+main {
+  height: 100vh;
+  width: 100%;
+  background: linear-gradient(180deg, rgba(255, 255, 255, 0.16) 0%, rgba(255, 255, 255, 0) 100%),
+    var(--color-black);
+}
+</style>
